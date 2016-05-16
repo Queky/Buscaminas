2550c261a39007be6a8e19bbddd28dec8021cfdd
Queky <inakisanchez_92@hotmail.com>
2016-05-16 12:49:58 +0200

    implementada bd

---
+++ src/Model/ConexionBaseDatos.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class ConexionBaseDatos {
+	
+	  public static void guardarUsuario()
+	  {
+	    Connection con = null;
+	    Statement st = null;
+	    Usuario u = Usuario.getUsuario();
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      con = DriverManager.getConnection("jdbc:sqlite:./BaseDatos/buscaminas.db");
+	      System.out.println("Opened database successfully");
+	      String sql = "insert into usuarios (nombre, puntuacion, dificultad) values ('"+u.getNombre()+"', '"+u.getPuntuacionMaxima()+"', '"+u.getNivelElegido()+"')";
+	      
+	      st = con.createStatement();
+	      st.executeUpdate("insert into usuarios (nombre, puntuacion, dificultad) values ('inaki', '2000', '3')");
+	      st.close();
+	      con.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	  }
+	  
+	  public static void main(String[] args) {
+		guardarUsuario();
+	}
+}
